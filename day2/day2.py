61f7684952e46d99bd29040a57f31e3da31fe14d
Adam Fallon <adamf@Adams-MacBook-Air.local>
2022-12-02 05:52:55 +0000

    Day 2

--- day2/day2.py
+++ day2/day2.py
@@ -2,25 +2,36 @@ import sys
 sys.path.append('./')
 import utils
 
-def solution(file):
-    data = utils.read_file_as_str_array(file)
-    d = {}
-    sum = 0
-    idx = 1
+strategy_1 = {
+    'A X': 1 + 3,
+    'A Y': 2 + 6,
+    'A Z': 3 + 0,
+    'B X': 1 + 0,
+    'B Y': 2 + 3,
+    'B Z': 3 + 6,
+    'C X': 1 + 6,
+    'C Y': 2 + 0,
+    'C Z': 3 + 3,
+}
 
-    for v in data:
-        if v == "":
-            d[idx] = sum
-            idx += 1
-            sum = 0
-        else:
-            sum += int(v)
-    
-    d = dict(sorted(d.items(), key=lambda x:x[1], reverse=True))
-    return list(d.values())[:3]
+strategy_2 = {
+    'A X': 3 + 0,
+    'A Y': 1 + 3,
+    'A Z': 2 + 6,
+    'B X': 1 + 0,
+    'B Y': 2 + 3,
+    'B Z': 3 + 6,
+    'C X': 2 + 0,
+    'C Y': 3 + 3,
+    'C Z': 1 + 6,
+}
+
+def main(data: list, strategy: dict) -> int:
+    sum = 0
+    for round in data:
+        sum += strategy[round]
+    return sum
 
 if __name__ == "__main__":
-    one = solution("./day2/day2.txt")
-    two = solution("./day2/day2.txt")
-    print(max(one))
-    print(sum(two))
+    print(main(utils.read_file_as_str_array("./day2/day2.txt"), strategy_1))
+    print(main(utils.read_file_as_str_array("./day2/day2.txt"), strategy_2))
